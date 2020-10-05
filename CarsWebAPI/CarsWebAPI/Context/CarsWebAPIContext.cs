afeeb9eb855d2d23a4843687059b46db3ccdf476
asiutra <adamsiutra@onet.pl>
2020-10-05 10:26:53 +0200

    Configure project: * Added needed package * changed application url to 8888 withou ssl * prepared DB and connectionString in appsettings.json * configured startup class

---
+++ CarsWebAPI/CarsWebAPI/Context/CarsWebAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsWebAPI.Context
+{
+    public class CarsWebAPIContext : DbContext
+    {
+        public CarsWebAPIContext(DbContextOptions<CarsWebAPIContext> options) : base(options) {  }
+
+    }
+}
--- CarsWebAPI/CarsWebAPI/Startup.cs
+++ CarsWebAPI/CarsWebAPI/Startup.cs
@@ -2,20 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CarsWebAPI.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 
 namespace CarsWebAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile("appsettings.json");
+            Configuration = config.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CarsWebAPIContext>(builder =>
+                builder.UseSqlServer(Configuration.GetConnectionString("SQL")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +46,9 @@ namespace CarsWebAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
